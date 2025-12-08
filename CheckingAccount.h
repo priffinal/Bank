e514bd2bab5cff0c5b8d8bbe913994edbbf91535
priffinal <vutruongson122345@gmail.com>
2025-12-08 16:38:02 +0700

    Update CheckingAccount.h

--- CheckingAccount.h
+++ CheckingAccount.h
@@ -9,7 +9,7 @@ private:
 	double transactionFee; // phi giao dich
 	double overdraftLimit; // gioi han rut qua tai khoan
 public:
-	void calculateInterest(); // tinh lai suat (lai suat nho)
-	void withdraw(double amount); // rut tien voi phi giao dich va gioi han rut qua tai khoan
+	double calculateInterest(); // tinh lai suat (lai suat nho)
+	bool withdraw(double amount); // rut tien voi phi giao dich va gioi han rut qua tai khoan
 };
 
